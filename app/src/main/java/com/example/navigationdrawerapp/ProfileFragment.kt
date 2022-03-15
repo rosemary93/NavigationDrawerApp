e2cbe3053087339b70b0ca3afc03ebbedb957302
rosemary <m.j.pourmoghadam@gmail.com>
2022-03-15 18:07:06 +0330

    ProfileFragment.kt and SettingFragment.kt added with their xmls. view binding set in them.nav_graph created both following fragments added in nav_graph.xml

---
+++ app/src/main/java/com/example/navigationdrawerapp/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navigationdrawerapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigationdrawerapp.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+   lateinit var binding:FragmentProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding= FragmentProfileBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/navigationdrawerapp/SettingFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigationdrawerapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigationdrawerapp.databinding.FragmentSettingBinding
+
+
+class SettingFragment : Fragment() {
+    lateinit var binding: FragmentSettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentSettingBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+
+
+}
