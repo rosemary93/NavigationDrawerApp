adeea900e453031bc71c2be8eaf5373d78308d9b
rosemary <m.j.pourmoghadam@gmail.com>
2022-03-15 17:09:08 +0330

    HomeFragment.kt added, view binding set. fragment_home.xml added. drawer_menu.xml added in res and items and its icons set. tag fragment container added in activity_main.xml. constraint layout changed to drawer layout. 4 icons added in drawable.an implementation added in dependencies in build.gradle

---
+++ app/src/main/java/com/example/navigationdrawerapp/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navigationdrawerapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigationdrawerapp.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    lateinit var binding:FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding= FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+
+}
